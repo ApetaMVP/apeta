83fe4cb9887546a06c5208051507de9d4301dec2
Ashar Nadeem <ashar@shipveho.com>
2023-03-24 14:39:46 -0400

    redirect to /site from base /

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,11 @@
+import { LoaderArgs, redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
+import { requireAuth } from "~/server/auth";
 
 export default function Index() {
   return <Outlet />;
 }
+
+export const loader = async ({ request }: LoaderArgs) => {
+  return (await requireAuth(request)) ? redirect("/site") : null;
+};
