fe14e0452b6b4b05d8a98299f4c4bb70e54b9e8b
Jessica Au <75815677+jessica-au@users.noreply.github.com>
2023-04-24 23:53:19 -0700

    Update logout.tsx

--- app/routes/auth/logout.tsx
+++ app/routes/auth/logout.tsx
@@ -1,15 +1,6 @@
 import { LoaderArgs } from "@remix-run/node";
-  import { logout } from "~/server/auth.server";
-  
+import { logout } from "~/server/auth.server";
   
 export async function loader({request}: LoaderArgs) {
   return await logout(request)
 }
-  
-  export default function Logout() {
-    return (
-      <>
-      </>
-    );
-  }
-  
