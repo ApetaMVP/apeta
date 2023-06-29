f11becf50670ed90f004eceb0815da71327a5e62
Cam Sima <cjsima@gmail.com>
2023-06-29 12:12:54 -0400

    fix for initial submit of form sending video file to server

--- app/routes/site/upload.tsx
+++ app/routes/site/upload.tsx
@@ -80,6 +80,7 @@ export default function Upload() {
   useEffect(() => {
     const uploadVideo = async () => {
       setLoading(true);
+      console.log({ actionData });
       const resp = await fetch(actionData?.uploadUrl!, {
         method: "PUT",
         body: file,
@@ -130,7 +131,6 @@ export default function Upload() {
         <Stack>
           <FileInput
             label="Video"
-            name="video"
             accept="video/*"
             icon={<IconUpload size={rem(14)} />}
             onChange={(e) => {
