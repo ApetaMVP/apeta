ce042d0413f3d06dbcb1e5df10cff3e3beff8ac1
Cam Sima <cjsima@gmail.com>
2023-08-24 12:27:35 -0400

    add privacy policy and tos

---
+++ app/routes/tos/index.tsx
@@ -0,0 +1,76 @@
+export default function Tos() {
+  return (
+    <body>
+      <h1>Terms of Service</h1>
+      <p>Last Updated: 8/23/23</p>
+
+      <p>
+        By using the <strong>Apeta</strong> mobile application (the "App"), you
+        agree to comply with and be bound by these Terms of Service ("Terms").
+        Please read these Terms carefully before using the App.
+      </p>
+
+      <h2>Acceptance of Terms</h2>
+      <p>
+        By accessing or using the App, you acknowledge that you have read,
+        understood, and agree to be bound by these Terms. If you do not agree
+        with these Terms, please do not use the App.
+      </p>
+
+      <h2>Use of the App</h2>
+      <p>
+        You agree to use the App only for lawful purposes and in a manner
+        consistent with these Terms. You are responsible for any content you
+        upload, share, or otherwise provide through the App.
+      </p>
+
+      <h2>Intellectual Property</h2>
+      <p>
+        The App and its content, including videos, graphics, logos, and other
+        materials, are protected by intellectual property laws. You may not
+        reproduce, modify, distribute, or create derivative works without our
+        written permission.
+      </p>
+
+      <h2>Privacy</h2>
+      <p>
+        Your use of the App is also governed by our Privacy Policy, which you
+        can find at [link to privacy policy]. Please review the Privacy Policy
+        to understand how we collect, use, and safeguard your information.
+      </p>
+
+      <h2>Termination</h2>
+      <p>
+        We reserve the right to suspend or terminate your access to the App if
+        you violate these Terms or engage in any behavior that we deem
+        inappropriate or harmful.
+      </p>
+
+      <h2>Disclaimer of Warranties</h2>
+      <p>
+        The App is provided "as is" without warranties of any kind. We make no
+        guarantees regarding the accuracy, reliability, or availability of the
+        App's content or features.
+      </p>
+
+      <h2>Limitation of Liability</h2>
+      <p>
+        We shall not be liable for any direct, indirect, incidental,
+        consequential, or punitive damages arising from your use of the App.
+      </p>
+
+      <h2>Changes to Terms</h2>
+      <p>
+        We may update these Terms from time to time. Any changes will be posted
+        on this page, and the "Last Updated" date at the top of the page will
+        indicate the date of the latest revision.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions, concerns, or requests regarding these Terms,
+        please contact us at josh@apeta.dev.
+      </p>
+    </body>
+  );
+}
